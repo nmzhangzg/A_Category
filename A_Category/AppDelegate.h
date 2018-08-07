1d566612931802d72ce513649ee6698480ed73c3
zzg <zhiguo-zhang@163.com>
2018-08-07 15:41:20 +0800

    Initial Commit

---
+++ A_Category/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  A_Category
+//
+//  Created by zhangzhiguo on 2018/8/7.
+//  Copyright © 2018年 zhangzhiguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ A_Category/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  A_Category
+//
+//  Created by zhangzhiguo on 2018/8/7.
+//  Copyright © 2018年 zhangzhiguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
